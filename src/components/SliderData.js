7b883b73090fdc34b8ceec2a3e2a2df2b85e7e86
lobotskiy <lobotskiy09@gmail.com>
2021-03-30 14:52:08 +0300

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://image.pbs.org/video-assets/EpdqJM1-asset-mezzanine-16x9-WROhWU0.jpg'
+    },
+    {
+        image: 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752'
+    },
+    {
+        image: 'https://static.onecms.io/wp-content/uploads/sites/20/2020/09/11/nature.jpg'
+    },
+    {
+        image: 'http://www.whistlingvacations.com/tourImages/NICE-&-PARIS-5-NIGHTS-6-DAYS-france.jpg'
+    },
+    {
+        image: 'https://www.visitmelbourne.com/-/media/images/great-ocean-road/things-to-do/nature-and-wildlife/lakes-and-waterways/hopetoun-falls_gor_r_supplied_credit-carolyn-hall_1600x900.jpg?ts=20170807270706'
+    }
+];
